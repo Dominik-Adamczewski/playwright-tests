530e1160e3f05cc943685a7eb54be96b29c6826f
Dominik-Adamczewski <dominik_adamczewski@wp.pl>
2026-06-27 08:11:05 +0200

    Temporarily disable retrying

--- conduit-app-e2e-testing/playwright.config.ts
+++ conduit-app-e2e-testing/playwright.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: 0,
+  workers: process.env.CI ? 2 : undefined,
   reporter: 'html',
   use: {
     baseURL: process.env.BASE_URL || 'http://localhost:3000',
