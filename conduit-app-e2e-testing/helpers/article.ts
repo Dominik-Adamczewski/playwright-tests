164c541a0e739b5def7464c9bd92c27a1d3092d2
Dominik-Adamczewski <dominik_adamczewski@wp.pl>
2026-06-29 06:12:46 +0200

    Fix base url in article.ts helper

--- conduit-app-e2e-testing/helpers/article.ts
+++ conduit-app-e2e-testing/helpers/article.ts
@@ -1,7 +1,7 @@
 import { APIRequestContext, expect } from "@playwright/test"
 import { NewArticle } from "../types/conduit-types";
 
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:3001';
 
 export const addNewArticleViaApi = async (
   requestFixture: APIRequestContext,
